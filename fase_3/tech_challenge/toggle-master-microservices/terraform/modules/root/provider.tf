2616cfd098fe67807499f5db72dcfccc9c69631b
rodx64 <rodrigo.bertini@gmail.com>
2026-03-09 17:31:36 -0300

    tf+cicd: remove dependencia circular do provider

--- fase_3/tech_challenge/toggle-master-microservices/terraform/modules/root/provider.tf
+++ fase_3/tech_challenge/toggle-master-microservices/terraform/modules/root/provider.tf
@@ -4,20 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0.0, < 6.0.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
   }
 }
 
 provider "aws" {}
-
-provider "kubernetes" {
-  host = try(data.aws_eks_cluster.this[0].endpoint, "")
-  cluster_ca_certificate = try(
-    base64decode(data.aws_eks_cluster.this[0].certificate_authority[0].data),
-    ""
-  )
-  token = try(data.aws_eks_cluster_auth.this[0].token, "")
-}
