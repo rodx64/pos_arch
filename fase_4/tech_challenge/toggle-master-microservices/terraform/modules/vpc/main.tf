219784860b187739c7f4e516dae3d91a038b416f
rodx64 <rodrigo.bertini@gmail.com>
2026-04-09 18:39:39 -0300

    intra: ajusta modulo vpc

--- fase_4/tech_challenge/toggle-master-microservices/terraform/modules/vpc/main.tf
+++ fase_4/tech_challenge/toggle-master-microservices/terraform/modules/vpc/main.tf
@@ -77,7 +77,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  count          = length(aws_subnet.public)
+  count          = length(var.public_subnets)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
@@ -96,7 +96,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private_assoc" {
-  count          = length(aws_subnet.private)
+  count          = length(var.private_subnets)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
