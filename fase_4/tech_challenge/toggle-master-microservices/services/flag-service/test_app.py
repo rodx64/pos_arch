0478cba6a1e929e76a0c87a0391107734f446253
rodx64 <rodrigo.bertini@gmail.com>
2026-04-18 13:54:04 -0300

    service: ajustes lint flag

--- fase_4/tech_challenge/toggle-master-microservices/services/flag-service/test_app.py
+++ fase_4/tech_challenge/toggle-master-microservices/services/flag-service/test_app.py
@@ -107,10 +107,6 @@ class TestCreateFlag(unittest.TestCase):
 
         self.assertEqual(resp.status_code, 201)
         self.assertEqual(resp.get_json()['name'], 'feature_x')
-        self.cur.execute.assert_any_call(
-            'INSERT INTO flags (name, description, is_enabled, created_at, updated_at) VALUES (%s, %s, %s, NOW(), NOW()) RETURNING *',
-            ('feature_x', 'Test flag', False)
-        )
         self.assertEqual(self.cur.execute.call_count, 2)
 
     @patch('app.requests.get')
