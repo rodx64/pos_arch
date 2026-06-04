4a5438caf665e236098f635c8a545ec9ca5980a7
rodx64 <rodrigo.bertini@gmail.com>
2026-06-04 19:50:22 -0300

    tf: ajuste de formatação

--- fase_5/tech_challenge/solidary-tech/terraform/modules/db-migrations/main.tf
+++ fase_5/tech_challenge/solidary-tech/terraform/modules/db-migrations/main.tf
@@ -22,17 +22,17 @@ resource "kubernetes_job_v1" "flyway_migration" {
           image = each.value.image
 
           env {
-            name  = "FLYWAY_URL"
+            name = "FLYWAY_URL"
             # [2] = Host, [3] = Porta, [4] = Banco de Dados
             value = "jdbc:postgresql://${local.parsed_urls[each.key][2]}:${local.parsed_urls[each.key][3]}/${local.parsed_urls[each.key][4]}"
           }
           env {
-            name  = "FLYWAY_USER"
+            name = "FLYWAY_USER"
             # [0] = Usuário (com urldecode para remover caracteres especiais como %21)
             value = urldecode(local.parsed_urls[each.key][0])
           }
           env {
-            name  = "FLYWAY_PASSWORD"
+            name = "FLYWAY_PASSWORD"
             # [1] = Senha (descodificada)
             value = urldecode(local.parsed_urls[each.key][1])
           }
