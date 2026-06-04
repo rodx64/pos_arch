e7fa68e26ff21262e245bdb61cea1877e597c0ca
rodx64 <rodrigo.bertini@gmail.com>
2026-06-04 19:21:31 -0300

    tf: ajuste de teste para token migration

--- fase_5/tech_challenge/solidary-tech/terraform/modules/db-migrations/provider.tf
+++ fase_5/tech_challenge/solidary-tech/terraform/modules/db-migrations/provider.tf
@@ -12,5 +12,5 @@ provider "kubernetes" {
   host                   = var.eks_tunnel_host != "" ? var.eks_tunnel_host : var.eks_cluster_endpoint
   cluster_ca_certificate = var.eks_tunnel_host != "" ? "" : base64decode(var.eks_cluster_ca)
   insecure               = var.eks_tunnel_host != ""
-  token                  = var.eks_cluster_token
+  token                  = trimspace(var.eks_cluster_token)
 }
