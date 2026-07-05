be5d060d6be48674a2fdc4d6aaa3a1901fb60885
rodx64 <rodrigo.bertini@gmail.com>
2026-07-05 17:55:02 -0300

    Revert "trigger: front"
    This reverts commit 43906146b5896177b7f9ea36a111fb0f8ff0ee18.

--- fase_5/tech_challenge/solidary-tech/front/app.js
+++ fase_5/tech_challenge/solidary-tech/front/app.js
@@ -270,7 +270,6 @@ function loadAll() {
   loadDonations();
 }
 
-// 
 window.addEventListener('DOMContentLoaded', () => {
   if (typeof window.APP_CONFIG !== 'undefined' && window.APP_CONFIG.API_URL) {
     BASE = window.APP_CONFIG.API_URL.replace(/\/$/, '');
