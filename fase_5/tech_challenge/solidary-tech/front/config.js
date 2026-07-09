83945ca22a577fcb18af180562bf5e3a850f3150
rodx64 <rodrigo.bertini@gmail.com>
2026-07-09 17:03:04 -0300

    eks: update ingress

--- fase_5/tech_challenge/solidary-tech/front/config.js
+++ fase_5/tech_challenge/solidary-tech/front/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-    API_URL: "http://a1440e89a378844f0b0ee3579103cd35-85d4bbd90fe80f6e.elb.us-east-1.amazonaws.com"
+    API_URL: "a1440e89a378844f0b0ee3579103cd35-85d4bbd90fe80f6e.elb.us-east-1.amazonaws.com"
 };
